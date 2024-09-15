695104b1d402401c595393394ffa7c7f52a97b90
Medlakhbabi <102824128+Medlakhbabi@users.noreply.github.com>
2024-09-15 13:14:37 +0100

    Add files via upload
    c06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molakhba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 20:44:26 by molakhba          #+#    #+#             */
+/*   Updated: 2024/09/15 09:10:33 by molakhba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		ft_putchar(str[i++]);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && s1[i] == s2[i])
+		i++;
+	return (s1[1] - s2[i]);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = 1;
+		while (j < ac - 1)
+		{
+			if (ft_strcmp(av[j], av[j + 1]) > 0)
+				ft_swap(&av[j], &av[j + 1]);
+			j++;
+		}
+		i++;
+	}
+	j = 1;
+	while (ac > j)
+	{
+		ft_putstr(av[j]);
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
+}
